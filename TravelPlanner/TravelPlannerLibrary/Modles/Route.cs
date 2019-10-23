012b2ec12160c537aa2b8068de591e945dccfbb2
Max Gusenbauer <max.gusenb@gmail.com>
2019-10-23 19:37:16 +0200

    Finished Homework

---
+++ TravelPlanner/TravelPlannerLibrary/Modles/Route.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelPlannerLibrary.Modles
+{
+    public class Route
+    {
+        public string Depart { get; set; }
+        public string DepartureTime { get; set; }
+        public string Arrive { get; set; }
+        public string ArrivalTime { get; set; }
+    }
+}
---
+++ TravelPlanner/TravelPlannerLibrary/Modles/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelPlannerLibrary.Modles
+{
+
+    public class Schedule
+    {
+        public string City { get; set; }
+
+        public DepTime[] ToLinz { get; set; }
+
+        public DepTime[] FromLinz { get; set; }
+    }
+
+    public class DepTime
+    {
+        public string Leave { get; set; }
+
+        public string Arrive { get; set; }
+    }
+}
